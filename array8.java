a90358a87d0cb62d61d5098d6df94fcce7bd7423
Anchal Singh <anchalsingh8736@gmail.com>
2025-09-29 18:51:08 +0530

    Add files via upload

---
+++ array8.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class  array8
+{
+    public static void main(String args[])
+    {
+        int a[]=new int[10];
+        int i=0,j=0,k=0;
+        Scanner sc=new Scanner(System.in);
+        for(i=0;i<10;i++)
+        {
+            System.out.println("enter the number");
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<10;i++)
+        {
+          for(j=i+1;j<10;j++)
+          {
+            if(a[i]>a[j])
+            {
+             k=a[i];
+             a[i]=a[j];
+             a[j]=k;
+            }
+          }
+        }
+        System.out.println("output is");
+        for(i=0;i<10;i++)
+        System.out.print(" "+a[i]);
+    }
+}    
+  
