9702fdf747d6257203996fc87b703377dcadfb11
Anchal Singh <anchalsingh8736@gmail.com>
2025-10-04 18:46:03 +0530

    Add files via upload

--- arr2D3.java
+++ arr2D3.java
@@ -4,8 +4,11 @@ class arr2D3
     public static void main(String ar[])
     {
      int a[][]=new int[3][3];
+     int b[][]=new int[3][3];
+     int c[][]=new int[3][3];
      int i=0,j=0;
      Scanner sc=new Scanner(System.in);
+     System.out.println("enter the first array");
      for(i=0;i<3;i++)
      {
          for(j=0;j<3;j++)
@@ -14,16 +17,31 @@ class arr2D3
              a[i][j]=sc.nextInt();
          }
      }
+     System.out.println("enter the second array");
      for(i=0;i<3;i++)
      {
          for(j=0;j<3;j++)
          {
-             if(a[i][j]%7==0||a[i][j]%10==7)
-             System.out.print(" "+a[i][j]+"buzz");
-             else
-             System.out.print(" "+a[i][j]+"nonbuzz");
+             System.out.println("enter the no.");
+             b[i][j]=sc.nextInt();
+         }
+     }
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             c[i][j]=a[i][j]+b[i][j];
+         }
+     }
+     System.out.println("output is");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             System.out.print(" "+c[i][j]);
          }
          System.out.println();
      }
     }
-}    
+}
+            
