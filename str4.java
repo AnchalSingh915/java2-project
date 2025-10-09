e0978800b7d107b03cb88131347e05bfe82f8b9f
Anchal Singh <anchalsingh8736@gmail.com>
2025-10-09 19:15:59 +0530

    Add files via upload

--- str4.java
+++ str4.java
@@ -1,32 +1,42 @@
 import java.util.*;
 class str4
-{
-    public static void main(String args[])
-    {
-        String str;
-        int i=0,j=0,k=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the string");
-        str=sc.nextLine();  
-        System.out.print(str.charAt(0));
-        for(i=0;i<str.length();i++)
+{    
+        public static void main (String arr[])
         {
-            if(str.charAt(i)==' ')
-            k=k+1;
-        }
-        for(i=0;i<str.length();i++)
-        {
-          if(str.charAt(i)==' ')
-          {
-              j=j+1;
-              if(j<=k)
-              System.out.print(".");
-              System.out.print(str.charAt(i+1));
-          }  
-        else
-        for(j=i+1;j<str.length();j++)
-        {  
-        System.out.print(str.charAt(i+1));
-         }
+            Scanner sc=new Scanner (System.in);
+            String str;
+            char ch;
+            int c=0,c1=0,i=0,j=0;
+            System.out.print("enter the string");
+            str=sc.nextLine();
+            
+            for(i=0;i<str.length();i++)
+            {
+                ch=str.charAt(i);
+                if(ch==' ')
+                c=c+1;
+            }
+            System.out.print(str.charAt(0)+".");
+              for(i=0;i<str.length();i++)
+              {
+                  ch=str.charAt(i);
+                if(ch==' ')
+                {
+                   c1=c1+1;
+                  if(c1<c)
+                   System.out.print(str.charAt(i+1)+".");
+                  else
+                  {
+                      for(j=i+1;j<str.length();j++)
+                      {
+                      System.out.print(str.charAt(j));
+                      }
+                  }    
+                }
+              }
+      }
 }
-
+              
+    
+                  
+              
