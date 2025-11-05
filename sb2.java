6632595066f775044139bbce03e30cb57778f7e6
Anchal Singh <anchalsingh8736@gmail.com>
2025-11-05 19:25:42 +0530

    Add files via upload

---
+++ sb2.java
@@ -0,0 +1,13 @@
+ import java.util.*;
+class sb2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int v=0,t=0;
+        StringBuffer s=new StringBuffer("30");
+        s.append(" infomax computer academy");
+        System.out.println(s);
+    }
+}
+            
